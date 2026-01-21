42525d753ba77a5bbf03e5fe87320f7e388feda9
AshwathNagarajan <ashwathnagarajan14@gmail.com>
2026-01-21 21:43:22 +0530

    Dataset Inserted

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+data = pd.read_csv('concrete.csv')
+print(data.head())
+print(data.info())
